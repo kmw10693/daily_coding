bade71dc9cd5fdb9837678f6677679b12068ac02
minu kim <kmw106933@naver.com>
2022-02-05 18:37:44 +0900

    스파르타코딩클럽: Spring 심화반

---
+++ 2022-02-05/springmvc/src/main/java/com/example/sparta/Star.java
@@ -0,0 +1,13 @@
+package com.example.sparta;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Star {
+    String name;
+    int age;
+}
